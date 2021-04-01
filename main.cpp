fdfa3a1b95cff2ead8bdbc21f17ffe94fc32e0b4
Bat-Chen Rothenberg <batcheni89@gmail.com>
2021-04-01 11:39:57 +0300

    minor bugfix: set timer on before initialize solvers

--- main.cpp
+++ main.cpp
@@ -64,8 +64,8 @@ int main(int argc, char * argv[]) {
     }
 
     MEGASampler s(argv[argc-1], max_samples, max_time, max_epoch_samples, max_epoch_time, strategy);
-    s.initialize_solvers();
 	s.set_timer_on("total");
+    s.initialize_solvers();
     s.set_timer_on("initial_solving");
     s.check_if_satisfiable();
     s.accumulate_time("initial_solving");
