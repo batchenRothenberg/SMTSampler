e96da10ecce73edbe90a1ed15cc37cd215ecdf26
Bat-Chen Rothenberg <batcheni89@gmail.com>
2021-04-01 12:15:51 +0300

    megasampler: nnf and simplify, API changed

--- megasampler.cpp
+++ megasampler.cpp
@@ -5,6 +5,28 @@ MEGASampler::MEGASampler(std::string input, int max_samples, double max_time, in
     	std::cout<<"starting MEGA"<<std::endl;
 }
 
+void MEGASampler::initialize_solvers(){
+	std::cout<<"MEGA initializing solvers"<<std::endl;
+	nnf_and_simplify_formula();
+    opt.add(simpl_formula); //adds formula as hard constraint to optimization solver (no weight specified for it)
+    solver.add(simpl_formula); //adds formula as constraint to normal solver
+}
 
+void MEGASampler::nnf_and_simplify_formula() {
+    z3::tactic simplify(c, "simplify");
+    z3::params p(c);
+    p.set("arith_lhs",true);
+    z3::tactic simp_with_params = with(simplify, p);
+    z3::tactic nnf(c, "nnf");
+    z3::tactic t_both = nnf & simp_with_params;
+    z3::goal g(c);
+    g.add(original_formula);
 
+    set_timer_on("conversion");
+    z3::apply_result res = t_both(g);
+    simpl_formula = res.as_expr();
+	std::cout<<res.as_expr()<<std::endl;
+	accumulate_time("conversion");
+
+}
 
--- megasampler.h
+++ megasampler.h
@@ -9,22 +9,9 @@ class MEGASampler : public Sampler {
 
 public:
     MEGASampler(std::string input, int max_samples, double max_time, int max_epoch_samples, double max_epoch_time, int strategy);
-    /*
-     * Finds additional valid models (samples) of the formula
-     * (based on the given model, which is assumed valid).
-     * Adds all valid samples to the samples set.
-     * Returns the number of new samples found.
-     */
-    int sample(const z3::model & model);
-    /*
-     * Saves to the given file a list of all samples found during calls to sample.
-     * File should already exist upon calling the function.
-     */
-    void save_valid_samples(std::string file);
-    /*
-     * Prints (to stdout) a list of all samples found during calls to sample.
-     */
-    void print_valid_samples();
+    void initialize_solvers();
+protected:
+    void nnf_and_simplify_formula();
 };
 
 
